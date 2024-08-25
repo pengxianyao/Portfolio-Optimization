adf9fb45309f7b460e3e127eaadc5c234ea5c0d8
Peng Xianyao <pengxianyao@gmail.com>
2024-08-25 18:11:58 +0800

    Initial commit

---
+++ xianyao/test.py
@@ -0,0 +1,6 @@
+print("\033[31mThis text is red!\033[0m")
+print("\033[32mThis text is green!\033[0m")
+print("\033[33mThis text is yellow!\033[0m")
+print("\033[34mThis text is blue!\033[0m")
+print("\033[35mThis text is magenta!\033[0m")
+print("\033[36mThis text is cyan!\033[0m")
